365854dd853a8fc84f8cb320650f3bcf25703f1e
Ignaciom <ignacio.camillucci@gmail.com>
2020-06-19 16:24:37 -0300

    Initial commit

---
+++ src/foreachclase/ForeachClase.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foreachclase;
+
+/**
+ *
+ * @author Notebook
+ */
+public class ForeachClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        int[] miArray = {1,2,3,4,5};
+        
+        
+        for (int i : miArray){
+            System.out.println(i);
+        }
+		
+		
+    }
+    
+}
